b4ec2078d1ebae8e7d8c7d177a1a85d8b73fb039
Igoris Cicinskas <igoris.cic@gmail.com>
2022-08-05 11:45:49 +0300

    05_aug

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import "./App.css";
-import { useState } from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddPost from "./components/AddPost";
 import AllPosts from "./components/AllPosts";
 import Toolbar from "./components/Toolbar";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
   return (
     <div className="app">
       <BrowserRouter>
         <Toolbar />
         <Routes>
-          <Route
-            path="/"
-            element={<AddPost posts={posts} setPosts={setPosts} />}
-          />
-          <Route
-            path="/allPosts"
-            element={<AllPosts posts={posts} setPosts={setPosts} />}
-          />
+          <Route path="/" element={<AddPost />} />
+          <Route path="/allPosts" element={<AllPosts />} />
         </Routes>
       </BrowserRouter>
     </div>
