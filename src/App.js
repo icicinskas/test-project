319d813b70dd6bd45dda027edc80e8780b8375ed
Igoris Cicinskas <igoris.cic@gmail.com>
2023-02-23 09:25:21 +0200

    23_01_23

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App main">
+    <div className="App">
       <MainContext.Provider value={state}>
         <BrowserRouter>
           <Toolbar />
